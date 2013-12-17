d5f75bed60fe5e3493ef8a439f5e59bd956ba21b
Greg Keane <gkeane@gmail.com>
2013-12-17 13:35:05 -0500

    first commit

---
+++ faculty_cv.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Faculty CV
+Plugin URI: http://wp.tutsplus.com/
+Description: Declares a plugin that will create a custom post type displaying movie reviews.
+Version: 1.0
+Author: Soumitra Chakraborty
+Author URI: http://wp.tutsplus.com/
+License: GPLv2
+*/
+?>
